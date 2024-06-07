d0ee0f7e8134bd61da497fab4cdb53aba453dcf0
m00605046 <metin.ilhan1@huawei.com>
2024-06-07 10:46:32 +0300

    Init

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use std::fmt::{Debug, Error};
+
+struct Mtn {
+pub name: String,
+  age: i32,
+  options: Choices,
+}
+
+impl Debug for Mtn {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    f.debug_struct("User")
+      .field("name", &self.name)
+      .field("age", &self.age)
+      .finish()
+  }
+}
+
+impl Mtn {
+ fn getName(&self)->String
+ {
+let s =  &self.name;
+s.to_string()
+ }
+    
+}
+enum Choices {
+  A,
+  B,
+  C,
+}
+
+pub const PUZZLE: u32 = 5;
+fn main() {
+  println!("Hello, world!");
+  let user = Mtn {
+    name: String::from("Metin"),
+    age: 35,
+    options: Choices::A,
+  };
+
+  let ss: String = format!("{user:#?}");
+}
+
+
